effa9674154a6f3a0b09dda060a0ee91cd43f422
Thanasak1412 <Thanasak.sr@snru.ac.th>
2024-10-31 02:18:51 +0700

    Splits a deck of cards into two parts: one of the specified hand size and the remaining cards.

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 // main initializes a new deck of cards, saves it to a file named "cards.txt",
 // and returns if an error occurs during the file-saving process.
 func main() {
 	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+
+	fmt.Println("Hand: ", hand)
+	fmt.Println("Remaining cards: ", remainingCards)
+
 	err := cards.saveToFile("cards.txt")
 	if err != nil {
 		return
